2ea63eaf348df63c7cf75a1088751f4586fb2f2a
student <student@iMac-student.local>
2020-05-18 22:25:16 +0300

    Fixes memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: tturnber <tturnber@MSK.21-SCHOOL.RU>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 15:37:16 by tturnber          #+#    #+#             */
-/*   Updated: 2020/05/13 14:52:25 by student          ###   ########.fr       */
+/*   Updated: 2020/05/18 22:23:35 by student          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,7 @@ void	*ft_memchr(void *str, int val, size_t size)
 			i++;
 		}
 	}
+	if ((char)val == '\0')
+		return ((void *)&str[i]);
 	return (NULL);
 }
